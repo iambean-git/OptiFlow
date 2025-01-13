99a983e4126b1cc787a9d6f085f6a21939cc1e8f
iambean-git <peb1021@naver.com>
2025-01-13 16:26:44 +0900

    first commit

---
+++ OptiFlowBE/src/main/java/com/optiflow/OptiFlowApplication.java
@@ -0,0 +1,13 @@
+package com.optiflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptiFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptiFlowApplication.class, args);
+	}
+
+}
