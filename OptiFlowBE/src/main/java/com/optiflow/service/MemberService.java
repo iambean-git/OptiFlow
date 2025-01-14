6c102cb332dd2f0c072166549e0ded39ef55f22d
wonny725 <wonny725@gmail.com>
2025-01-14 18:15:17 +0900

    250114_member_CRUD생성

---
+++ OptiFlowBE/src/main/java/com/optiflow/service/MemberService.java
@@ -0,0 +1,44 @@
+package com.optiflow.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.optiflow.domain.Member;
+import com.optiflow.persistence.MemberRepository;
+
+@Service
+public class MemberService {
+	
+	@Autowired
+	private MemberRepository memberRepo;
+	
+	@Autowired
+	private PasswordEncoder encoder;
+	
+
+	public Member saveMember(Member member) {
+    	Member memberRegi = Member.builder()
+                .username(member.getUsername())
+                .password(encoder.encode(member.getPassword()))
+                .role(Member.Role.User)
+                .build();
+		
+		return memberRepo.save(memberRegi);
+	}
+
+	public List<Member> getAllMembers() {
+		return memberRepo.findAll();
+	}
+
+	public Optional<Member> getMemberByUsername(String username) {
+		return memberRepo.findByUsername(username);
+	}
+
+	public void deleteMember(String username) {
+		memberRepo.deleteByUsername(username);
+	}
+}
