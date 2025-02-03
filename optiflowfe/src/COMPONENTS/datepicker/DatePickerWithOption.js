04d6804cfce916f7350fca9bae7ed9fe009b2440
iambean-git <peb1021@naver.com>
2025-02-03 18:12:08 +0900

    25-02-03 정수장별 유출량 그래프

--- optiflowfe/src/COMPONENTS/datepicker/DatePickerWithOption.js
+++ optiflowfe/src/COMPONENTS/datepicker/DatePickerWithOption.js
@@ -5,10 +5,12 @@ import DatePicker from "react-datepicker";
 import { ko } from 'date-fns/locale';
 import "react-datepicker/dist/react-datepicker.css";
 import "./datepickerwithoption.css"
-export default function DatePickerWithOption() {
-    const [option, setOption] = useState("day"); // "day", "month", "time"
+export default function DatePickerWithOption({setDateOption}) {
+    const [option, setOption] = useState("daily"); // "daily", "monthly", "hourly"
     const [selectedDate, setSelectedDate] = useState(null);
-    const [maxDate, setMaxDate] = useState(new Date());
+    const [maxDate, setMaxDate] = useState(() => {
+        return new Date(2024, 9, 16, 23, 59, 59);
+    });
 
     function range(start, end, step = 1) {
         const result = [];
@@ -25,11 +27,11 @@ export default function DatePickerWithOption() {
     ];
 
     const ExampleCustomInput = forwardRef(
-        ({ value, onClick}, ref) => (
+        ({ value, onClick }, ref) => (
             <div className="bg-white w-[200px] h-[40px] rounded-md flex items-center relative justify-center" onClick={onClick} ref={ref}>
                 <FaRegCalendar className="absolute left-2 text-[#7c8bdb]" />
                 <span className="pl-2">
-                    {value || (option === "time" ? "날짜를 선택하세요" : (option === "month" ? "연도를 선택하세요" : "월을 선택하세요"))} {/* 기본 텍스트 설정 */}
+                    {value || (option === "hourly" ? "날짜를 선택하세요" : (option === "month" ? "연도를 선택하세요" : "월을 선택하세요"))} {/* 기본 텍스트 설정 */}
                 </span>
             </div>
         ),
@@ -41,57 +43,43 @@ export default function DatePickerWithOption() {
     };
 
 
-    useEffect(() => {
-        // const yesterday = new Date();
-        // yesterday.setDate(yesterday.getDate() - 1); // 하루 전으로 설정
-        // yesterday.setHours(10, 0, 0, 0); // 오전 10시로 설정 (분, 초, 밀리초는 0으로 초기화)
-        // setSelectedDate(yesterday);
-
-        const maxdate = new Date();
-        maxdate.setDate(maxdate.getDate() - 1); // 하루 전으로 설정
-        maxdate.setHours(23, 59, 59, 999); // 어제의 끝으로 설정
-        setMaxDate(maxdate);
-    }, []);
-
     // 선택된거 확인
     useEffect(() => {
         if (!selectedDate) return;
         const year = selectedDate.getFullYear();
         const month = String(selectedDate.getMonth() + 1).padStart(2, "0");
         const day = String(selectedDate.getDate()).padStart(2, "0");
-        // const hours = String(selectedDate.getHours()).padStart(2, "0");
-        // const minutes = String(selectedDate.getMinutes()).padStart(2, "0");
-        // const seconds = String(selectedDate.getSeconds()).padStart(2, "0");
 
         const selected = {
             option: option,
             selectedValue: (
-                option === "time" ? `${year}-${month}-${day}` :
-                    option === "month" ? `${year}` :
+                option === "hourly" ? `${year}-${month}-${day}` :
+                    option === "monthly" ? `${year}` :
                         `${year}-${month}`
             )
         };
-        console.log("selected :", selected);
+        // console.log("selected :", selected);
+        setDateOption(selected);
     }, [selectedDate]);
 
     return (
         <div className="flex items-center">
             <section className="flex h-12 bg-gray-100 p-0.5 w-fit border border-[#7c8bdb] rounded-md hover:cursor-pointer">
                 <div className={`p-2 rounded-tl-md rounded-bl-md border-r w-16 text-center
-                                ${option === 'time' ? "bg-[#7c8bdb] text-white" : "bg-white text-black"}`}
-                    onClick={() => handleOptionChange("time")}
+                                ${option === 'hourly' ? "bg-[#7c8bdb] text-white" : "bg-white text-black"}`}
+                    onClick={() => handleOptionChange("hourly")}
                 >
                     시간별
                 </div>
                 <div className={`p-2 w-16 text-center 
-                                ${option === 'day' ? "bg-[#7c8bdb] text-white" : "bg-white text-black"}`}
-                    onClick={() => handleOptionChange("day")}
+                                ${option === 'daily' ? "bg-[#7c8bdb] text-white" : "bg-white text-black"}`}
+                    onClick={() => handleOptionChange("daily")}
                 >
                     일별
                 </div>
                 <div className={`p-2 rounded-tr-md rounded-br-md border-l w-16 text-center
-                                ${option === 'month' ? "bg-[#7c8bdb] text-white" : "bg-white text-black"}`}
-                    onClick={() => handleOptionChange("month")}
+                                ${option === 'monthly' ? "bg-[#7c8bdb] text-white" : "bg-white text-black"}`}
+                    onClick={() => handleOptionChange("monthly")}
                 >
                     월별
                 </div>
@@ -99,7 +87,7 @@ export default function DatePickerWithOption() {
             <div className="ml-2 bg-gray-50 p-0.5 w-52 h-12 rounded-md border border-[#7c8bdb]">
                 <DatePicker
                     renderCustomHeader={
-                        option === "time" ?
+                        option === "hourly" ?
                             ({
                                 date,
                                 changeYear,
@@ -156,15 +144,15 @@ export default function DatePickerWithOption() {
                     onChange={(date) => setSelectedDate(date)}
                     customInput={<ExampleCustomInput />}
                     dateFormat={
-                        option === "day"
+                        option === "daily"
                             ? "yyyy년 MM월"
-                            : option === "month"
+                            : option === "monthly"
                                 ? "yyyy년"
                                 : "yyyy년 MM월 dd일"
                     }
-                    showYearPicker={option === "month"}
-                    showMonthYearPicker={option === "day"}
-                    timeIntervals={60} // 시간별 선택 시 15분 단위로 설정
+                    showYearPicker={option === "monthly"}
+                    showMonthYearPicker={option === "daily"}
+                    hourlyIntervals={60} // 시간별 선택 시 15분 단위로 설정
                     locale={ko}
                     maxDate={maxDate}
                     minDate={new Date("2023-01-01")}
--- optiflowfe/src/PAGES/Regions.js
+++ optiflowfe/src/PAGES/Regions.js
@@ -9,14 +9,41 @@ export default function Regions() {
   const { kakao } = window;
   const [map, setMap] = useState(null);
   const [container, setContainer] = useState(null);
-  const [graphTitle, setGraphTitle] = useState("J 배수지");
+  const [graphTitle, setGraphTitle] = useState("J");
+  const [dateOption, setDateOption] = useState(null);
+  const [graphData, setGraphData] = useState(null);
+  useEffect(() => {
+    if (!dateOption) return;
+    // console.log("🗺 [Regions] dateOption : ", dateOption);
+    fetchWaterOutFlowData(dateOption);
+  }, [dateOption, graphTitle]);
+
+
+  // 배수지별 유출량 데이터
+  const fetchWaterOutFlowData = async (dateOption) => {
+    const url = `http://10.125.121.226:8080/api/reservoirdata/${dateOption.option}/${dateOption.selectedValue}/${graphTitle.toLowerCase()}`;
+    console.log("🌊 [Regions] url : ", url);
+    const resp = await fetch(url);
+    const data = await resp.json();
+    // console.log("🌊 [Regions] 유출량 데이터 :", data);
+
+    if (!data) return;
+
+    const result = {};
+    data.map((item, index) => {
+      result[index + 1] = item.totalOutput.toFixed(2);
+    });
+
+    console.log("🌊 [Regions] 유출량 데이터 graphData :", result);
+    setGraphData(result);
+  }
 
   // 지도 생성
   useEffect(() => {
     const containerOBJ = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
     setContainer(containerOBJ);
     const options = { //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(35.8044830568065, 126.91), //지도의 중심좌표.
+      center: new kakao.maps.LatLng(35.7992, 126.9260), //지도 첫 로드시 중심좌표.
       level: 10 //지도의 레벨(확대, 축소 정도)
     };
 
@@ -62,8 +89,6 @@ export default function Regions() {
       { type: "reservoir", label: "H", positon: [35.70, 127.16] },
     ];
 
-
-
     markersInfo.map((m) => {
       const markerimgSrc = m.type == "reservoir" ? "/images/marker_blue.png" : "/images/marker_red.png";
       const markerimageSize = m.type == "reservoir" ? new kakao.maps.Size(42, 45) : new kakao.maps.Size(50, 54); // 마커이미지의 크기
@@ -83,7 +108,7 @@ export default function Regions() {
 
       // 글자
       const blueIwContent = `<div id=${m.label} class="pointer-events-none text-white relative bottom-10 right-[6px] font-bold" >${m.label}</div>`;
-      const redIwContent =`<div class="px-4 py-1 text-xs relative top-3 left-[-3px] bg-white rounded-md">${m.label}</div>`;
+      const redIwContent = `<div class="px-4 py-1 text-xs relative top-3 left-[-3px] bg-white rounded-md ">${m.label}</div>`;
 
       const markerOverlay = new kakao.maps.CustomOverlay({
         map: map,
@@ -102,9 +127,9 @@ export default function Regions() {
   }, []);
 
   const clickMarkers = (label) => {
-    if(label=="정수장") return;
+    if (label == "정수장") return;
     console.log(label);
-    setGraphTitle(label+" 배수지");
+    setGraphTitle(label);
   };
 
   const Window = ({ label }) =>
@@ -112,10 +137,6 @@ export default function Regions() {
       onClick={() => console.log("hello :", label)}>
       {label}
     </div>
-
-  useEffect(()=>{
-    console.log("[Regions] 그래프타이틀 : ", graphTitle);
-  },[]);
 
   //주소를 지오코드로 변환
   const addrToGeo = () => {
@@ -137,27 +158,12 @@ export default function Regions() {
   }
   //지도 정보 얻어오는 함수
   const getMapInfo = () => {
-    // 지도의 현재 중심좌표를 얻어옵니다 
-    const center = map.getCenter();
-
-    // 지도의 현재 레벨을 얻어옵니다
-    const level = map.getLevel();
-
-    // 지도타입을 얻어옵니다
-    const mapTypeId = map.getMapTypeId();
-
-    // 지도의 현재 영역을 얻어옵니다 
-    const bounds = map.getBounds();
-
-    // 영역의 남서쪽 좌표를 얻어옵니다 
-    const swLatLng = bounds.getSouthWest();
-
-    // 영역의 북동쪽 좌표를 얻어옵니다 
-    const neLatLng = bounds.getNorthEast();
-
-    // 영역정보를 문자열로 얻어옵니다. ((남,서), (북,동)) 형식입니다
-    const boundsStr = bounds.toString();
-
+    const center = map.getCenter();           // 지도의 현재 중심좌표를 얻어옵니다  
+    const level = map.getLevel();             // 지도의 현재 레벨을 얻어옵니다
+    const mapTypeId = map.getMapTypeId();     // 지도타입을 얻어옵니다
+    const bounds = map.getBounds();           // 지도의 현재 영역을 얻어옵니다 
+    const swLatLng = bounds.getSouthWest();   // 영역의 남서쪽 좌표를 얻어옵니다 
+    const neLatLng = bounds.getNorthEast();   // 영역의 북동쪽 좌표를 얻어옵니다 
 
     let message = '지도 중심좌표는 위도 ' + center.getLat() + ', <br>';
     message += '경도 ' + center.getLng() + ' 이고 <br>';
@@ -174,12 +180,13 @@ export default function Regions() {
       <NavBar />
       <div className="w-full h-screen pl-[260px] flex flex-col">
         <section className="w-full h-[160px] px-10 flex justify-between items-end">
-          <div className="h-full flex items-end">
-            <h1 className="text-3xl">타이틀</h1>
-            {/* <p className="text-xs mb-[-1rem]">상기 배수장 및 배수지의 위치 정보는 참고용으로 제공된 것으로, 실제 위치와는 차이가 있을 수 있습니다.</p> */}
+          {/* 텍스트 */}
+          <div className="w-2/5 h-full  flex flex-col justify-end text-[#333333]">
+            <h1 className="text-4xl ">타이틀</h1>
+            <p className="mt-2">각 배수지를 클릭하면, <span className="whitespace-nowrap"> 세부 정보를 확인할 수 있습니다. **멘트수정필요**</span></p>
           </div>
           <button onClick={getMapInfo} className="bg-blue-300 font-Freesentation font-light">지도정보보기</button>
-          <DatePickerWithOption/>
+          <DatePickerWithOption setDateOption={setDateOption} />
         </section>
 
         <div className="px-10 pb-10 pt-6 w-full h-full flex">
@@ -191,15 +198,19 @@ export default function Regions() {
 
           <div className="pl-3 w-fit">
             {/* ===== 그래프1 ===== */}
-            <section className="h-1/2 pb-4 w-[600px]">
-              <div className="w-full h-full border-black bg-white flex justify-center items-center">
-                <WaterOutFlowGraph graphTitle={graphTitle} />
+            <section className="h-1/2 pb-4 w-[700px]">
+              <div className="w-full h-full border-black bg-white flex justify-center items-center pt-4">
+                {
+                  graphData ?
+                    <WaterOutFlowGraph graphTitle={graphTitle} data={graphData} datepickerOption={dateOption && dateOption.option} />
+                    :
+                    <div className="text-gray-500"> 날짜를 선택하세요 </div>
+                }
               </div>
             </section>
             {/* ===== 그래프2 ===== */}
-            <section className="h-1/2 pt-4 w-[600px]">
+            <section className="h-1/2 pt-4 w-[700px]">
               <div className="w-full h-full border-black bg-white ">
-                <Window label="hello" />
               </div>
             </section>
           </div>
