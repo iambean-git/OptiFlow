303ccce5aa0ec165a024a7cd02e02a3483103b97
iambean-git <peb1021@naver.com>
2025-02-07 17:39:49 +0900

    25-02-07 main 수정

--- optiflowfe/src/COMPONENTS/mainComponents/MainComponent3.js
+++ optiflowfe/src/COMPONENTS/mainComponents/MainComponent3.js
@@ -3,21 +3,29 @@ import "./MainComponent.css";
 export default function MainComponent3() {
   return (
     <div className="inner">
-      <video
+
+<div className="w-2/5 h-full relative overflow-hidden"> {/* 부모 div에 relative 적용 */}
+    <video
         src="/videos/수돗물.mp4"
         loop
         muted
         autoPlay
-        className="background-video" // 백그라운드 비디오 클래스 이름 변경
-      />
-      <img src="/images/logo_square.png" className="size-96" alt="logo" />
-      <section className="text-center">
+        className="absolute top-0 left-0 w-full h-full object-cover scale-x-[-1] z-[-1] scale-y-[1.2]"
+    />
+</div>
+
+
+      <div className="w-3/5 h-full">
+
+      </div>
+
+      {/* <section className="text-center">
         <p>
           {" "}
           어둠 속에서 빛나는 효율, 스마트 솔루션이 에너지 패러다임을 바꿉니다.
         </p>
         <p></p>
-      </section>
+      </section> */}
     </div>
   );
 }
